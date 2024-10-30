7b25f662caffe1fd1b41a7ade7a54f2385bdf6bd
siddhim03 <siddhimittal2004@gmail.com>
2024-10-29 23:26:09 -0500

    added pay button functionality

--- client/src/views/CashierView/CashierView.js
+++ client/src/views/CashierView/CashierView.js
@@ -3,6 +3,7 @@ import Receipt from './Receipt';
 import CategoryTabs from './CategoryTabs';
 import OrderControls from './OrderControls';
 import './CashierView.css';
+import Pay from './Pay';
 
 const CashierView = () => {
   const [selectedCategory, setSelectedCategory] = useState('Bowl');
@@ -33,6 +34,13 @@ const CashierView = () => {
     const newItem = { name: item, price };
     setReceipt([...receipt, newItem]);
     setTotal(total + price);
+  };
+
+  const handlePay = () => {
+    // Logic to handle payment confirmation
+    alert('Your order has been placed!'); // Simple alert as a placeholder
+    setReceipt([]); // Clear receipt
+    setTotal(0); // Reset total
   };
 
   return (
@@ -84,7 +92,7 @@ const CashierView = () => {
           ))}
         </div>
 
-        <OrderControls />
+        <OrderControls onPay={handlePay} />
       </div>
     </div>
   );
--- client/src/views/CashierView/OrderControls.js
+++ client/src/views/CashierView/OrderControls.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const OrderControls = () => {
+const OrderControls = ({ onPay }) => {
   return (
     <div className="order-controls">
       <button>Print Last Receipt</button>
       <button>Add Discount</button>
       <button>Split Bill</button>
       <button>No Tax</button>
-      <button className="checkout-button">Pay / Close Order</button>
+      <button className="checkout-button" onClick={onPay}> Pay / Close Order</button>
     </div>
   );
 };
