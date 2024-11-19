ed141669f6033fe943c7480629e2978f193ac7a9
meenalikasingh <157546536+meenalikasingh@users.noreply.github.com>
2024-11-19 15:19:20 -0600

    Add files via upload

--- client/src/views/CustomerView/Biggerplate.js
+++ client/src/views/CustomerView/Biggerplate.js
@@ -1,51 +1,50 @@
-import React, { useState } from "react";
-import "./CustomerView.css";
+import React, {} from 'react';
+import '../../App.css';
+import './CustomerView.css';
 
-const DrinkSelection = () => {
-    const [selected, setSelected] = useState("");
-
-    const drinks = ["Fountain", "Mexican Coke", "Apple Juice", "Water Bottle"];
-
-    const handleSelect = (drink) => {
-        setSelected(drink);
-    };
-
-    const handleAdd = () => {
-        if (selected) {
-            alert(`You added: ${selected}`);
-        } else {
-            alert("Please select a drink!");
-        }
-    };
-
+function Biggerplate() {
     return (
-        <div className="customer-layout">
+        <div className="plate-layout">
             <div className="title-bar">
-                <h1>Select Your Drink</h1>
+                <h1>Bigger Plate</h1>
             </div>
-            <div className="button-container">
-                {drinks.map((item) => (
-                    <button
-                        key={item}
-                        className={`category-circle ${
-                            selected === item ? "entree-circle" : ""
-                        }`}
-                        onClick={() => handleSelect(item)}
-                    >
-                        {item}
-                    </button>
-                ))}
+
+            <div className="middle-section">
+                <div className="category-description">
+                    <div className="sides-circle">
+                        <span>Sides</span>
+                    </div>
+                    <p>Select your Sides</p>
+                </div>
+
+                <div className="category-description">
+                    <div className="entree-circle">
+                        <span>Entree</span>
+                    </div>
+                    <p>Select your 1st Entree</p>
+                </div>
+
+                <div className="category-description">
+                    <div className="entree-circle">
+                        <span>Entree</span>
+                    </div>
+                    <p>Select your 2nd Entree</p>
+                </div>
+
+                <div classNae="category-description">
+                    <div className="entree-circle">
+                        <span>Entree</span>
+                    </div>
+                    <p>Select your 3rd Entree</p>
+                </div>
             </div>
+
             <div className="bottom-bar">
-                <button className="cancel-button" onClick={() => setSelected("")}>
-                    Cancel
-                </button>
-                <button className="add-button" onClick={handleAdd}>
-                    Add
-                </button>
+                <button className="cancel-button">Cancel</button>
+                <button className="add-button">Add</button>
             </div>
         </div>
     );
-};
+}
 
-export default DrinkSelection;
+export default Biggerplate;
