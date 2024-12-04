ba37365b587a1a69a4bb8e61b6ca8cfbab4885f0
siddhim03 <siddhimittal2004@gmail.com>
2024-12-04 15:19:31 -0600

    changed file names

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import ManageItems from "./views/ManagerView/ManageItems";
 import Reports from "./views/ManagerView/MonthlyStatistics";
 
 import Drink from "./views/CustomerView/Drink";
-import Appetizer from "./views/CustomerView/Appetizer";
+import Appetizer from "./views/CustomerView/Appetizer"; //don't know why its giving error
 import BiggerPlate from "./views/CustomerView/Biggerplate";
 import Entree from "./views/CustomerView/Entree";
 import Side from "./views/CustomerView/Side";
