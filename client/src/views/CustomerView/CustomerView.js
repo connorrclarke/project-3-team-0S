cbb5bb222e0a3f266e2d26374dd0db15d4823309
meenalikasingh <meenalika.singh@tamu.edu>
2024-12-05 19:18:25 -0600

    Layout fixed, added a seperate css for front page to prevent issues

--- client/src/views/CustomerView/CustomerView.js
+++ client/src/views/CustomerView/CustomerView.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './CustomerView.css';
+import './Customeronly.css';
 import Receipt from './ReceiptKiosk';
 import {useZoom, ZoomProvider} from "./ZoomContext";
 
